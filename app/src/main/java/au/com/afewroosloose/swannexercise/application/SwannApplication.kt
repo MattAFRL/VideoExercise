23f1bed77ad4ef7ddb5de83d84ad651149e36257
Matthew Potter <matthewpotter@Matthews-Air.modem>
2024-07-06 12:48:19 +1000

    Initial commit

---
+++ app/src/main/java/au/com/afewroosloose/swannexercise/application/SwannApplication.kt
@@ -0,0 +1,8 @@
+package au.com.afewroosloose.swannexercise.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SwannApplication: Application() {
+}
